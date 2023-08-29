baf545ceb39ff10b1d7d2ed288edeff61e8c02b6
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-08-29 15:05:32 +0530

    commit FAQ

--- script.js
+++ script.js
@@ -4,3 +4,23 @@
     menu.classList.toggle("active");
   }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButtons = document.querySelectorAll(".toggle");
+
+    toggleButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const faqItem = button.parentNode;
+            const answer = faqItem.querySelector(".answer");
+
+            if (answer.style.display === "none") {
+                answer.style.display = "block";
+                button.textContent = "x";
+                faqItem.querySelector(".question").style.color = "#1CC382"; // Change question color to green
+            } else {
+                answer.style.display = "none";
+                button.textContent = "+";
+                faqItem.querySelector(".question").style.color = ""; // Reset question color
+            }
+        });
+    });
+});
