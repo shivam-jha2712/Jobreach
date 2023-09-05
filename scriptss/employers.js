f155426912fb920b3290d879c965cec4c1ead078
Prankush Giri <76916192+prankush-tech@users.noreply.github.com>
2023-09-06 03:15:04 +0530

    navbar-Done

--- scriptss/employers.js
+++ scriptss/employers.js
@@ -5,13 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     burgerMenu.addEventListener("click", function () {
         menu.classList.add("active");
+        burgerMenu.style.opacity=0
         burgerMenu.classList.add("active");
         closeMenu.classList.add("active");
     });
-
+    
     closeMenu.addEventListener("click", function () {
         menu.classList.remove("active");
         burgerMenu.classList.remove("active");
         closeMenu.classList.remove("active");
+        burgerMenu.style.opacity=1
     });
 });
+
--- scriptss/seekers.js
+++ scriptss/seekers.js
@@ -7,11 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
         menu.classList.add("active");
         burgerMenu.classList.add("active");
         closeMenu.classList.add("active");
+        burgerMenu.style.opacity=0
     });
-
+    
     closeMenu.addEventListener("click", function () {
         menu.classList.remove("active");
         burgerMenu.classList.remove("active");
+        burgerMenu.style.opacity=1
         closeMenu.classList.remove("active");
     });
 });
+
