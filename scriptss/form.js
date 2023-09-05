fc89e97dab0e3fed11c3f8cdbf6fc443cde9c4c9
Prankush Giri <76916192+prankush-tech@users.noreply.github.com>
2023-09-06 04:21:21 +0530

    get-hired on index

--- scriptss/form.js
+++ scriptss/form.js
@@ -1,2 +1,14 @@
 const hireButton =document.querySelectorAll(".button-onClick-gethired")
-console.log(hireButton)
+const popForm =document.querySelector(".pop-up-Positioning")
+const closebutton =document.querySelector(".closebutton")
+hireButton.forEach(button => {
+    button.addEventListener("click",()=>{
+        popForm.style.display = "flex"
+    })
+    
+});
+
+closebutton.addEventListener('click',()=>{
+    popForm.style.display = "none"
+
+})
