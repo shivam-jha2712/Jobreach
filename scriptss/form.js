cad5ad41e0077802fce51fb5e816a674542122ef
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-09-06 09:48:53 +0530

    commit

--- scriptss/form.js
+++ scriptss/form.js
@@ -1,7 +1,7 @@
-const hireButton =document.querySelectorAll(".button-onClick-gethired")
+const gethiredButton =document.querySelectorAll(".button-onClick-gethired")
 const popForm =document.querySelector(".pop-up-Positioning")
 const closebutton =document.querySelector(".closebutton")
-hireButton.forEach(button => {
+gethiredButton.forEach(button => {
     button.addEventListener("click",()=>{
         popForm.style.display = "flex"
     })
@@ -10,5 +10,23 @@ hireButton.forEach(button => {
 
 closebutton.addEventListener('click',()=>{
     popForm.style.display = "none"
+    
+})
 
+
+
+const hireTalentButton =document.querySelectorAll(".button-onClick-hireTalent")
+const popForm1 =document.querySelector(".pop-up-Positioning1")
+const closebutton1 =document.querySelector(".closebutton1")
+
+hireTalentButton.forEach(button => {
+    button.addEventListener("click",()=>{
+        popForm1.style.display = "flex"
+    })
+    
+});
+
+closebutton1.addEventListener('click',()=>{
+    popForm1.style.display = "none"
+    
 })
