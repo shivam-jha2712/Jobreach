a52e2cebeea01dce6e939428a794c447cd904cb9
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-09-07 12:03:00 +0530

    commit

--- scriptss/contact.js
+++ scriptss/contact.js
@@ -18,3 +18,36 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+const gethiredButton = document.querySelectorAll(".button-onClick-gethired")
+const popForm = document.querySelector(".pop-up-Positioning")
+const closebutton = document.querySelector(".closebutton")
+gethiredButton.forEach(button => {
+    button.addEventListener("click", () => {
+        popForm.style.display = "flex"
+    })
+
+});
+
+closebutton.addEventListener('click', () => {
+    popForm.style.display = "none"
+
+})
+
+
+
+const hireTalentButton = document.querySelectorAll(".button-onClick-hireTalent")
+const popForm1 = document.querySelector(".pop-up-Positioning1")
+const closebutton1 = document.querySelector(".closebutton1")
+
+hireTalentButton.forEach(button => {
+    button.addEventListener("click", () => {
+        popForm1.style.display = "flex"
+    })
+
+});
+
+closebutton1.addEventListener('click', () => {
+    popForm1.style.display = "none"
+
+})
